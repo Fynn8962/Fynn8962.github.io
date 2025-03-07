e8c20f825204b41eaf00e083c52c6daad94d10af
Fynn8962 <142886149+Fynn8962@users.noreply.github.com>
2025-03-07 15:42:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var i = 0;
+var speed = 50;
+var headerName = document.getElementById("name");
+var textName = headerName.innerHTML;
+headerName.innerHTML = "";
+
+function typeName(callback) {
+    if (i < textName.length) {
+      headerName.innerHTML += textName.charAt(i);
+      i++;
+      setTimeout(() => typeName(callback), speed);
+    } else if (callback) {
+        callback();
+    }
+  }
+
+var j = 0;
+speedPhrase = 40;
+var headerPhrase = document.getElementById("phrase");
+var textPhrase = headerPhrase.innerHTML;
+headerPhrase.innerHTML = "";
+
+
+function typePhrase() {
+    if (j < textPhrase.length) {
+      headerPhrase.innerHTML += textPhrase.charAt(j);
+      j++;
+      setTimeout(typePhrase, speedPhrase);
+    } 
+}
+
+
+
+window.onload = function() {
+    typeName(typePhrase); 
+    
+  };
